008e34bede91f75476a27e16a495472c1615b3a1
Huegelkoenig <46036679+Huegelkoenig@users.noreply.github.com>
2020-03-24 04:49:20 +0100

    added some methods to the Coords class

--- public/scripts/class_coords.js
+++ public/scripts/class_coords.js
@@ -21,11 +21,17 @@ constructor arguments:
   height... integer
 
 methods:
-  .add(summand): adds smmand values to the existing x, y, width and height values
-      summand... another coordinates object
-      returns the sum
   .changeTo(coords): changes values to given coodinates
-      coords... another coordinates object
+      coords... another Coords object
+  .add(summand): adds values to the existing x, y, width and height values
+      summand... another Coords object
+      returns the sum
+  .justAdd(summand): like add, but doesnt change the original values
+  .multiply(multiplier): multiplies the corrosponding x values, y values, width values and height values
+      multiplier... another Coords object
+      returns the product
+  .justMultiply(multiplier): like multiply, but doesnt change the original values
+  
 */
 
 class Coords extends Object{
@@ -37,6 +43,13 @@ class Coords extends Object{
     this.height = height;
   }
 
+  changeTo(coords){
+    this.x = coords.x;
+    this.y = coords.y;
+    this.width = coords.width;
+    this.height = coords.height;
+  }
+
   add(summand){
     this.x += summand.x;
     this.y += summand.y;
@@ -45,11 +58,24 @@ class Coords extends Object{
     return this;
   }
 
-  changeTo(coords){
-    this.x = coords.x;
-    this.y = coords.y;
-    this.width = coords.width;
-    this.height = coords.height;
+  justAdd(summand){
+    let sum = newCoords(this.x,this.y,this.width,this.height);
+    sum.add(summand);
+    return sum;
+  }
+
+  multiply(multiplier){
+    this.x *= multiplier.x;
+    this.y *= multiplier.y;
+    this.width *= multiplier.width;
+    this.height *= multiplier.height;
+    return this;
+  }
+
+  justMultiply(multiplier){
+    let product = newCoords(this.x,this.y,this.width,this.height);
+    product.multiply(multiplier);
+    return product;
   }
 }
 
@@ -76,4 +102,25 @@ function newCoords(x, y, width, height){
       throw Error(`class_coords.js: function newCoords: coordinates must be integer`);
   }
   return new Coords(x, y, width, height);
+}
+
+/*
+function newCoordsXY(x,y)
+
+description:
+  checks just the x and y arguments and creates new coordinates
+
+arguments:
+  x... integer
+  y... integer
+
+return:
+  if x and y arguments are ok, a new coordinate object with undefined width and height will be returned, else an error will be thrown
+*/
+
+function newCoordsXY(x, y){
+  if (!Number.isInteger(x) || !Number.isInteger(y)){
+      throw Error(`class_coords.js: function newCoords: coordinates must be integer`);
+  }
+  return new Coords(x, y, undefined, undefined);
 }
