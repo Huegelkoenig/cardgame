df96edeaaf22261b2e326e6ed8ce690d1326c6be
Huegelkoenig <46036679+Huegelkoenig@users.noreply.github.com>
2024-04-14 22:03:08 +0200

    some files were not staged for commit

--- my_modules/cardgame/cardgame.js
+++ my_modules/cardgame/cardgame.js
@@ -3,34 +3,34 @@
 const dbScripts = require("../db-scripts/db-scripts");
 
 function initGame(socket){
-  const listOfFilesToLoad = {list: [
-                             {type: 'image',  name: 'hearts',             size:  99}, //array of graphics and sounds + user-dependant graphics and sounds //TODO: insert real values
-                             {type: 'image',  name: '1',                  size: 100},
-                             {type: 'image',  name: 'hearts_highlighted', size: 116},
-                             {type: 'image',  name: 'spades',             size: 100},
-                             {type: 'image',  name: '2',                  size: 100},
-                             {type: 'image',  name: 'spades_highlighted', size: 116},
-                             {type: 'image',  name: 'clubs',              size: 102},
-                             {type: 'image',  name: 'clubs_highlighted',  size: 119},
-                             {type: 'image',  name: '3',                  size: 100},
-                             {type: 'image',  name: 'diamonds',           size: 101},
-                             {type: 'image',  name: 'a (1)',              size: 100},
-                             {type: 'image',  name: 'a (2)',              size: 100},
-                             {type: 'image',  name: 'a (3)',              size: 100},
-                             {type: 'image',  name: 'a (4)',              size: 100},
-                             {type: 'image',  name: 'a (5)',              size: 100},
-                             {type: 'image',  name: 'a (6)',              size: 100},
-                             {type: 'image',  name: 'a (7)',              size: 100},
-                             {type: 'image',  name: 'a (8)',              size: 100},
-                             {type: 'image',  name: 'a (9)',              size: 100},
-                             {type: 'image',  name: 'a (10)',             size: 100},
-                             {type: 'image',  name: 'a (11)',             size: 100},
-                             {type: 'image',  name: 'a (12)',             size: 100},
-                             {type: 'image',  name: 'diamonds_highlighted', size: 118},
-                             {type: 'image',  name: 'aatolex',            size: 22}]}; //append user specific imags or sounds
-  let imagesize = 0;
-  listOfFilesToLoad.list.forEach((el)=>{imagesize+=el.size});
-  listOfFilesToLoad.imagesize = imagesize;
+  const listOfFilesToLoad = {images: [{size:  99, name: 'hearts'              }, //array of graphics and sounds + user-dependant graphics and sounds //TODO: insert real values
+                                      {size: 100, name: '1'                   },
+                                      {size: 116, name: 'hearts_highlighted'  },
+                                      {size: 100, name: 'spades'              },
+                                      {size: 100, name: '2'                   },
+                                      {size: 116, name: 'spades_highlighted'  },
+                                      {size: 102, name: 'clubs'               },
+                                      {size: 119, name: 'clubs_highlighted'   },
+                                      {size: 100, name: '3'                   },
+                                      {size: 101, name: 'diamonds'            },
+                                      {size: 100, name: 'a (1)'               },//DELETE: large files just for testing
+                                      {size: 100, name: 'a (2)'               },
+                                      {size: 100, name: 'a (3)'               },
+                                      {size: 100, name: 'a (4)'               },
+                                      {size: 100, name: 'a (5)'               },
+                                      {size: 100, name: 'a (6)'               },
+                                      {size: 100, name: 'a (7)'               },
+                                      {size: 100, name: 'a (8)'               },
+                                      {size: 100, name: 'a (9)'               },
+                                      {size: 100, name: 'a (10)'              },
+                                      {size: 100, name: 'a (11)'              },
+                                      {size: 100, name: 'a (12)'              },
+                                      {size: 118, name: 'diamonds_highlighted'},
+                                      {size:  22, name: 'aatolex'             }],
+                              sounds: []}; //TODO: also append sounds and maybe user specific images or sounds
+  let totalSize = 0;
+  Object.values(listOfFilesToLoad).forEach((list)=>{list.forEach((el)=>{totalSize+=el.size})});
+  listOfFilesToLoad.totalSize = totalSize;
   socket.emit('connectionValidated', listOfFilesToLoad);
 
 
@@ -46,12 +46,13 @@ function initGame(socket){
     socket.emit('playerState', playerState);
   });
 
+  //DELETE:
   socket.on('test', ()=>{
     socket.emit('teest');
   });
 
 
-  //for testing
+  //DELETE:  just for testing
   socket.on('press',(x,y)=>{console.log(`${socket.username} pressed at x: ` + x + '   y: '+ y)});
   socket.on('release',(x,y)=>{console.log(`${socket.username} released at x: ` + x + '   y: '+ y)});
 }
